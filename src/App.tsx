6fc032ab9a1806da089ad0b27fca7901346e1d53
Yukiya Nakagawa <y.nakagawa@water-cell.jp>
2018-08-08 14:14:27 +0900

    Translate App with TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
+import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default class App extends Component {
+export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
